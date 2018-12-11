1cfa2d3f9c226e6f4defd60c871cff878a1763ca
Unknown <Jon_athan71@hotmail.com>
2018-12-11 13:09:17 +0100

    small update

--- beginner_tutorials/scripts/ai_robotics_project/agent_environment.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-class agent_environment:
-    def __init__(self):
-        # double array aanmaken
-        self.static_map = np.array([[.0 for _ in range(4)] for _ in range(4)])
-        # map vullen met xy "coordinaten/posities"
