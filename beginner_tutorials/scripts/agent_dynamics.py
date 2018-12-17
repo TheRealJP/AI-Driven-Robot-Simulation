aa89da6776259f743f7406445956748ce0f26ede
Unknown <Jon_athan71@hotmail.com>
2018-12-17 01:11:24 +0100

    testing msg callback function and behavior

--- beginner_tutorials/scripts/agent_dynamics.py
+++ beginner_tutorials/scripts/agent_dynamics.py
@@ -52,7 +52,7 @@ def avg_minimum(l, n_min):
                 max_min_dists = max(min_dists)
         n += 1
     dist = sum(min_dists) / n_min
-    rospy.loginfo('dist: %s, nbr of points %s', str(dist), str(n))
+    # rospy.loginfo('dist: %s, nbr of points %s', str(dist), str(n))
 
     return dist
 
@@ -60,6 +60,13 @@ def avg_minimum(l, n_min):
 # subscribers in commentaar --> geen actie meer, blijft wachten
 # dus init node name niet echt invloed
 # cmd_vel gebruiken..
+# does scan and odom work together nicely?
+# odom for precise tracking
+# scanner for not bumping into wall
+# -------
+# roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/gandalf/catkin_ws/src/ROS_Robotics/beginner_tutorials/worlds/static_boxes
+
+
 class Robot:
     def __init__(self, topic, threshold, linear_speed, angular_speed, rate, env):
         # Init
@@ -83,16 +90,16 @@ class Robot:
         # Direction & Rotationdatax
         self.robot_env = env
         self.robot_env.fill_optimal_path()
-        self.action = int(self.robot_env.direction_facing)  # first action
+        self.action = self.robot_env.direction_facing
+        self.angle = 0
+        self.next_position = 0
 
         # Position robot
-        # scan
-        self.__scanner = rospy.Subscriber('/scan', LaserScan, self.set_cmd_vel)  # i'm subscribing on this topic
+        # scan, for not bumping into wall
+        self.scanner = rospy.Subscriber('/kobuki/laser/scan', LaserScan,
+                                        self.set_cmd_vel)  # i'm subscribing on this topic
         rospy.loginfo('wait')
-        rospy.wait_for_message('/scan', LaserScan)
-
-        # odom
-        rospy.Subscriber('/odom', Odometry, self.get_odom)
+        rospy.wait_for_message('/kobuki/laser/scan', LaserScan)
 
         # Spin
         rospy.loginfo('spin')
@@ -105,42 +112,46 @@ class Robot:
 
     # callback scan
     def set_cmd_vel(self, msg):
-        rospy.loginfo('Turning: %s; Ticks: %s / %s', str(self.__turning), str(self.__current_tick), str(self.__ticks))
+        # rospy.loginfo(len(msg.ranges))
+        rospy.loginfo(msg.ranges[320])
+        # rospy.loginfo('one point: %s', msg.ranges[320])
+        # rospy.loginfo('multiple point: %s', avg_minimum(msg.ranges[315:330], len(msg.ranges[315:330])))  # straight in middle
+        # rospy.loginfo('Turning: %s; Ticks: %s / %s', str(self.__turning), str(self.__current_tick), str(self.__ticks))
 
-        # scan distance to wall from the camerapoint
+        # "dist" is scan_distance to the wall based from the camerapoint
         can_move, dist = self.scan(msg)
-        next_position = 0
         # scanner gives back total distance from where you stand
-        #
         # scan_dist - dist_todo = going_to_this_dist --> do this one time when you stand still (linear speed is 0...)
         # amount_todo_now >= scan_dist
 
-        # Move forward if possible and when not turning
-        if can_move and not self.__turning:
+        # Move forward if possible and when not turning and when you passed the set distance goal
+        if can_move and not self.__turning and self.next_position < dist:
 
             # if the speed is 0 measure the next point to reach
-            if self.__move_cmd.linear.x is 0:
-                next_position = dist - 1
+            if self.__move_cmd.linear.x == 0.0:
+                # update next distance
+                self.next_position = dist - 1
+                rospy.loginfo('next_position: %s', self.next_position)
 
-            # check if we reached goal
-            if next_position > dist:
+            # step gives back the next action based on the current action
+            current_state = self.robot_env.current_state
+            current_action = self.robot_env.optimal_path[current_state].action
+            self.action = self.robot_env.step(current_action)
 
-                # update to the next action
-                self.action = self.robot_env.step(self.action)
-
-            # todo: odometry one meter forward
-            rospy.loginfo('move forward')
+            # rospy.loginfo('move forward')
             self.__move_cmd.angular.z = 0
             self.__move_cmd.linear.x = self.__linear_speed
             self.__cmd_vel.publish(self.__move_cmd)
-
-
         # Else turn
         else:
+            # todo check if you still need to turn based on current radians
+
             rospy.loginfo('turn')
             self.__move_cmd.linear.x = 0
             self.__move_cmd.angular.z = self.__angular_speed
             self.turn()
+
+        msg.ranges = []
 
     def scan_distance(self, msg):
         dist = avg_minimum(msg.ranges, len(msg.ranges) / 10)
@@ -148,25 +159,29 @@ class Robot:
 
     def scan(self, msg):
         dist = avg_minimum(msg.ranges, len(msg.ranges) / 10)
+        # rospy.loginfo('msg distance: %s', avg_minimum(msg.ranges, len(msg.ranges) / 10))
         return dist > self.__threshold, dist
 
     #    signal that you have arrived (something like stopped its ticks)
     def turn(self):
+
         if self.__current_tick < 1:
             # returns radians to be turned with a given action
-            angle = self.robot_env.rotate(self.action)
-            rospy.loginfo(angle)
-            rospy.loginfo('turning %s radians (90 degrees)', angle)
-            angular_duration = angle / self.__angular_speed
+            self.angle = self.robot_env.rotate(self.action)
+
+            rospy.loginfo('turning %s radians (%s degrees)',
+                          self.angle, self.angle * 180 / math.pi)
+            angular_duration = self.angle / self.__angular_speed
             self.__ticks = int(angular_duration * self.__rate)
             self.__turning = True
             self.__current_tick = 1
+
         elif self.__current_tick >= self.__ticks:
             self.__current_tick = 0
             self.__turning = False
         else:
-            angle = pi / 2
-            rospy.loginfo('turning %s radians (90 degrees)', angle)
+            rospy.loginfo('turning %s radians (%s degrees)',
+                          self.angle, self.angle * 180 / math.pi)
             rospy.loginfo('turning at %s radians / s', str(self.__move_cmd.angular.z))
             self.__cmd_vel.publish(self.__move_cmd)
             self.__current_tick += 1
@@ -181,5 +196,8 @@ if __name__ == '__main__':
     try:
         env = AgentEnvironment(4, 4, 15)
         roomba = Robot('/mobile_base/commands/velocity', 1, .2, .3, 10, env)
+        roomba.scanner = rospy.Subscriber('/kobuki/laser/scan', LaserScan,
+                                          roomba.set_cmd_vel)  # i'm subscribing on this topic
+
     except:
         rospy.loginfo('Roomba node terminated.')
